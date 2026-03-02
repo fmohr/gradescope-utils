e21a3062cfe747f46dcefbab0fa2eb933fc15c13
Felix Mohr <mail@felixmohr.de>
2026-03-02 19:10:36 +0100

    enabled webp for better compression and smaller image codes

--- gradescope_utils/autograder_utils/test_class.py
+++ gradescope_utils/autograder_utils/test_class.py
@@ -8,6 +8,16 @@ class TestTemplate(unittest.TestCase):
     def plot_figure(self, fig):
 
         tmpfile = BytesIO()
-        fig.savefig(tmpfile, format='png')
+        #fig.savefig(tmpfile, format='png')
+        fig.savefig(
+            tmpfile,
+            format="webp",
+            dpi=100,
+            bbox_inches="tight",
+            pad_inches=0.02,
+            pil_kwargs={
+                "quality": 70,
+                "method": 6    # best compression, slower
+            })
         encoded = base64.b64encode(tmpfile.getvalue()).decode("iso-8859-1")
-        print(f'<img src="data:image/png;base64,{encoded}" />')
+        print(f'<img src="data:image/webp;base64,{encoded}" />')
